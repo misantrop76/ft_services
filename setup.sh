80b5a45ce780daf40e5534987e25e4ebb28d420b
misantrop76 <maty76600@outlook.fr>
2021-01-27 00:47:07 +0100

    hey

--- setup.sh
+++ setup.sh
@@ -1,15 +1,14 @@
 
 echo "setup minikube"
+#minikube delete
+minikube start --vm-driver=virtualbox
+eval $(minikube docker-env)
+minikube addons enable metallb
 
-minikube addons enable dashboard
-minikube addons enable ingress
-minikube start --vm-driver=virtualbox --extra-config=apiserver.service-node-port-range=1-10000
-
-
-
-echo "build images"
-
-#docker build --tag service_nginx ./srcs/nginx
+echo "\nbuild images"
+docker build -t nginx ./srcs/nginx/.
 #docker build -t service_mysql ./srcs/mysql
 #docker build -t service_wordpress ./srcs/wordpress
 #docker build -t service_phpmyadmin ./srcs/phpmyadmin
+kubectl apply -f srcs/nginx.yaml
+minikube dashboard &
